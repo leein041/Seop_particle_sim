eb3490980efe8fd84a1394aba1bed3721656b077
leein041 <lis15945678@gmail.com>
2026-01-13 13:34:08 +0900

    2026.01.13 / Input -> input 이름변경

---
+++ src/seop/input/seop_input/input.hpp
@@ -0,0 +1,59 @@
+#pragma once
+#include "keys.hpp"
+#include "seop_math/math.hpp"
+#include <vector>
+
+namespace seop {
+class Context;
+}
+
+namespace seop::input {
+class Key {
+  public:
+    Key_code  key_code{Key_code::End};
+    Key_state state{Key_state::None};
+    bool      pressed{false};
+    int       vk_key_code{0};
+};
+
+class Input_data {
+  public:
+    math::Vec2 pre_cursor_pos{0.0f, 0.0f};
+    math::Vec2 cur_cursor_pos{0.0f, 0.0f};
+    math::Vec3 ray_point{0.0f, 0.0f, 0.0f};
+};
+
+class Input {
+  public:
+    Input();
+    ~Input() = default;
+
+    void               init();
+    void               update(Context& ctx);
+    void               reset();
+    [[nodiscard]] auto get_delta_cursor_pos() const -> math::Vec2;
+    [[nodiscard]] auto get_key_down(Key_code code) -> bool;
+    [[nodiscard]] auto get_key_up(Key_code code) -> bool;
+    [[nodiscard]] auto get_key(Key_code code) -> bool;
+    [[nodiscard]] auto data() const -> const Input_data&;
+    [[nodiscard]] auto data() -> Input_data&;
+    void               set_ray_point(const math::Vec3& ray_point);
+
+    float mouse_sensitivity = 0.1f;
+
+  private:
+    void update_cursor_pos(Context& ctx);
+    void update_mouse(Context& ctx);
+    void update_mouse_button(Context& ctx, int glfw_button, Key_code code);
+    void update_keys(Context& ctx);
+    void update_key(Key& key, Context& ctx);
+    void update_key_down(Key& key);
+    void update_key_up(Key& key);
+    void create_key();
+    void clear_key();
+
+  private:
+    std::vector<Key> keys_;
+    Input_data       data_;
+};
+} // namespace seop::input
---
+++ src/seop/input/seop_input/keys.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+namespace seop::input
+{
+#define KEY_LIST(v) \
+    v(LButton, 0x01) \
+    v(RButton, 0x02) \
+    v(MButton, 0x04) \
+    v(Space, 0x20) \
+    v(End, 0x23) \
+    v(Left, 0x25) \
+    v(Up, 0x26) \
+    v(Right, 0x27) \
+    v(Down, 0x28) \
+    v(Delete, 0x2E) \
+    v(A, 0x41) \
+    v(B, 0x42) \
+    v(C, 0x43) \
+    v(D, 0x44) \
+    v(E, 0x45) \
+    v(F, 0x46) \
+    v(G, 0x47) \
+    v(H, 0x48) \
+    v(I, 0x49) \
+    v(J, 0x4A) \
+    v(K, 0x4B) \
+    v(L, 0x4C) \
+    v(M, 0x4D) \
+    v(N, 0x4E) \
+    v(O, 0x4F) \
+    v(P, 0x50) \
+    v(Q, 0x51) \
+    v(R, 0x52) \
+    v(S, 0x53) \
+    v(T, 0x54) \
+    v(U, 0x55) \
+    v(V, 0x56) \
+    v(W, 0x57) \
+    v(X, 0x58) \
+    v(Y, 0x59) \
+    v(Z, 0x5A) \
+    v(LeftShift, 0xA0) \
+    v(RightShift, 0xA1) \
+    v(LeftControl, 0xA2) \
+    v(RightControl, 0xA3) \
+    v(LeftAlt, 0xA4) \
+    v(RightAlt, 0xA5)
+
+enum class Key_state
+{
+    Down,
+    Pressed,
+    Up,
+    None,
+};
+enum class Key_code : int
+{
+#define ENUM_GEN(name, code) name = code,
+    KEY_LIST(ENUM_GEN)
+#undef ENUM_GEN
+};
+
+inline const char *get_key_string(Key_code key)
+{
+    switch (key) {
+#define NAME_GEN(name, code)                                                                                           \
+    case Key_code::name:                                                                                               \
+        return #name;
+
+        KEY_LIST(NAME_GEN)
+
+#undef NAME_GEN
+    default:
+        return "Unknown";
+    }
+}
+} // namespace seop::input
